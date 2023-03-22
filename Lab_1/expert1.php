019e4a118407d256244970b4cc112ab98b37b359
katekrema <91109791+kkrema@users.noreply.github.com>
2023-03-22 23:26:12 +0300

    menu href

--- Lab_1/expert1.php
+++ Lab_1/expert1.php
@@ -4,15 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <link rel="stylesheet" href="../css/main.css">
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
+    <title>Expert</title>
+    <link rel="stylesheet" href="http://172.28.22.160:8888/../css/main.css">
+    <link rel='stylesheet' href="http://172.28.22.160:8888/resources/font-awesome.min.css">
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
 </head>
 <body>
+    <?php 
+    session_start();
+    if(!isset($_SESSION['login'])){
+        $_SESSION['error'] = "[RIGHTS] Вам запрещен доступ на эту страницу!";
+        header('Location: '. 'http://172.28.22.160:8888/error.php');
+        exit();
+    }
+    ?>
 <nav role="navigation" class="nav">
         <div class="container">
             <div class="nav-row">
-                <a href="./index.html" class="logo"><strong>ИТМО</strong> university</a>
+                <a class="logo"><strong>ИТМО</strong> university</a>
                 <ul class="nav-list">
                     <li class="nav-list__item dropdown">
                         <a href="#" class="nav-list__link nav-list__link--active"><span>Lab_4</span></a>
@@ -36,10 +45,8 @@
                         <a href="#" class="nav-list__link nav-list__link--active"><span>Lab_2</span></a>
                         <nav class="submenu">
                             <ul class="submenu-items">
-                                <li class="submenu-item"><a href="#" class="submenu-link">Home</a></li>
-                                <li class="submenu-item"><a href="#" class="submenu-link">Color Test</a></li>
-                                <li class="submenu-item"><a href="#" class="submenu-link">Color Test</a></li>
-                                <li class="submenu-item"><a href="#" class="submenu-link">Color Test</a></li>
+                            <li class="submenu-item"><a href="../home2.html" class="submenu-link">Home</a></li>
+                                <li class="submenu-item"><a href="../expert2.html" class="submenu-link">Expert</a></li>
                             </ul>
                         </nav>
                     </li> 
@@ -47,8 +54,8 @@
                         <a href="#" class="nav-list__link nav-list__link--active"><span>Lab_1</span></a>
                         <nav class="submenu">
                             <ul class="submenu-items">
-                                <li class="submenu-item"><a href="#" class="submenu-link">Home</a></li>
-                                <li class="submenu-item"><a href="#" class="submenu-link">Expert</a></li>
+                                <li class="submenu-item"><a href="../home1.php" class="submenu-link">Home</a></li>
+                                <li class="submenu-item"><a href="../expert1.php" class="submenu-link">Expert</a></li>
                             </ul>
                         </nav>
                     </li> 
@@ -65,18 +72,17 @@
                     </li>  
                     <li class="nav-list__item" id="account">
                         <i class="fa fa-user"></i>
-                        <a href="./login.html" class="nav-list__link">Login</a>
+                        <a href="../login.html" class="nav-list__link">Login</a>
                         |
-                        <a href="./register.html" class="nav-list__link">Register</a>
+                        <a href="../register.html" class="nav-list__link">Register</a>
                     </li>
                 </ul>
             </div>    
         </div>    
     </nav>
-
     <main class="section">
         <div class="container">
-            <h2 class="title-1">Professions</h2>
+            <h2 class="title-1">Выберите профессию для оценки!</h2>
             <ul class="professions" id="professions">
                 <li class="profession">
                     <a href="./profession-page.html">
@@ -112,9 +118,9 @@
             </ul>
         </div>
     </main>
-
+    <!-- job_listing_area_end  dsddfdds-->
     <script src="../js/app.js"></script>
-    <script src="../js/lab_1/home.js"></script>
-    <script src="../js/lab_1/menu.js"></script>
+    <script src="../js/lab_1/expert_chose.js"></script>
+    <script src=".../js/menu.js"></script>
 </body>
 </html>
