49af087c7e409d77ddbeb73a84e80b665cdac24f
katekrema <91109791+kkrema@users.noreply.github.com>
2023-03-22 22:43:18 +0300

    site refurbishment...

---
+++ js/menu.js
@@ -0,0 +1,25 @@
+[].slice.call(document.querySelectorAll('.dropdown .nav-list__link')).forEach(function(el){
+    el.addEventListener('click', onClick, false);
+});
+
+function onClick(e){
+    e.preventDefault();
+    var el = this.parentNode;
+    el.classList.contains('show-submenu') ? hideSubMenu(el) : showSubMenu(el);
+}
+
+function showSubMenu(el){
+    el.classList.add('show-submenu');
+    document.addEventListener('click', function onDocClick(e){
+        e.preventDefault();
+        if(el.contains(e.target)){
+            return;
+        }
+        document.removeEventListener('click', onDocClick);
+        hideSubMenu(el);
+    });
+}
+
+function hideSubMenu(el){
+    el.classList.remove('show-submenu');
+}
